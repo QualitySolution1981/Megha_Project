0cce02919fa8b3141c94328bffa2eb7c3aa90d14
testingqualitysolution19-admin <testingqualitysolution+a100@gmail.com>
2025-11-24 12:42:00 +0530

    New class add

---
+++ Megha_Project/src/Megha/Arshad.java
@@ -0,0 +1,22 @@
+package Megha;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Arshad {
+ 
+	@BeforeTest
+	  public void beforeTest() {
+	  }
+	
+	@Test
+  public void bothvalueblank() {
+  }
+  
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
